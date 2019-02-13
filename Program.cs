64d8606da6ec1b7d2cc57376f75ae0c68796fd02
constant-i <constant-i@yandex.ru>
2019-02-13 22:57:47 +0300

    начинаем курс ООП урок 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MY Hello World in C#");
+            
+            Console.SetBufferSize(80,25);
+            Console.Read();
+        }
+    }
+}
