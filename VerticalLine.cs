b984fe4770d81acf60054baacdb81916e50f679b
constant-i <constant-i@yandex.ru>
2019-02-19 22:40:18 +0300

    класс реализован как наследник класса Figure, из класса убрано лишнее, что релаизует  Figure

--- VerticalLine.cs
+++ VerticalLine.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace MySnake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
-
         public VerticalLine( int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
@@ -19,14 +17,5 @@ namespace MySnake
                 pList.Add(p);
             }
         }
-
-        public void Draw ()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
-        }
-
     }
 }
