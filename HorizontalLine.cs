63f46792e4c9a62a383d856a7790901065c7af03
constant-i <constant-i@yandex.ru>
2019-02-19 22:39:08 +0300

    класс реализован как наследник класса Figure, убрано лишнее (что есть в Figure)

--- HorizontalLine.cs
+++ HorizontalLine.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace MySnake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pList;
-
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
@@ -19,15 +17,5 @@ namespace MySnake
                 pList.Add(p);
             }
         }
-
-
-        public void Draw ()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
-        }
-
     }
 }
