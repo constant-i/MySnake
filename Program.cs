587bc580c3e2326606c30b08d00453b2b0f14c56
constant-i <constant-i@yandex.ru>
2019-02-19 22:40:45 +0300

    добавлено отрисовка рамки

--- Program.cs
+++ Program.cs
@@ -12,16 +12,19 @@ namespace MySnake
         {
             Console.SetBufferSize(80,25);
 
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            rightLine.Draw();
+            leftLine.Draw();
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-
-            HorizontalLine hLine1 = new HorizontalLine(5, 10, 5, 'Y');
-            hLine1.Draw();
-
-            VerticalLine vLine = new VerticalLine(0, 1, 10, 'U');
-            vLine.Draw();
 
             Console.Read();
         } 
