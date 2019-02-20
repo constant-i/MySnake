8814ebe2615340ca649567adc7f455cbe396e266
constant-i <constant-i@yandex.ru>
2019-02-20 22:28:00 +0300

    в класс Точка добавлен: - конструктор точки, принимающий другую точку, - метод сдвинуть точку, принимающий смещение точки и направление - переопределён метод ToString

--- Point.cs
+++ Point.cs
@@ -24,11 +24,45 @@ namespace MySnake
             this.sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+            this.sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                // TODO // у перподавателя знаки вверх и вниз наоборот
+                y = y - offset;
+            }
+            else
+            {
+
+                y = y + offset;  // TODO // у перподавателя знаки вверх и вниз наоборот
+            }
+        }
 
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
+        }
+
     }
 }
