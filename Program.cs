67b2aaefb84eaf7086d871b376052b0c3f936323
constant-i <constant-i@yandex.ru>
2019-02-20 22:28:48 +0300

    проверка работы класса "Змейка" Snake

--- Program.cs
+++ Program.cs
@@ -21,10 +21,21 @@ namespace MySnake
             rightLine.Draw();
             leftLine.Draw();
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            Point p2 = new Point(12,12, '2');
+            Snake snake2 = new Snake(p2, 6, Direction.UP);
+            snake2.Draw();
+
+            Point p3 = new Point(13, 12, '3');
+            Snake snake3 = new Snake(p3, 6, Direction.DOWN);
+            snake3.Draw();
+
+            Point p4 = new Point(11, 12, '4');
+            Snake snake4 = new Snake(p4, 6, Direction.LEFT);
+            snake4.Draw();
 
             Console.Read();
         } 
