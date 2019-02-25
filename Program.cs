106b829076950485e03390e60f8a4aa1fe92c661
constant-i <constant-i@yandex.ru>
2019-02-25 19:39:12 +0300

    скрыл курсор добавлен бесконечный цикл с вызовом метода змейки для обработки клавиш

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace MySnake
         static void Main(string[] args)
         {
             Console.SetBufferSize(80,25);
+            Console.CursorVisible = false;
 
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
@@ -25,25 +26,17 @@ namespace MySnake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
 
-
-            Console.Read();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         } 
     }
 }
