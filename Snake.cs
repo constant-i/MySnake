f61259a2a084852e6733affe9dff457e2f12959b
constant-i <constant-i@yandex.ru>
2019-02-26 20:28:37 +0300

    привел код в точное соответствие с кодом у преподавателя,  начиная от порядка операторов, написания имён переменных и пробелов в сторках

--- Snake.cs
+++ Snake.cs
@@ -8,30 +8,26 @@ namespace MySnake
 {
     class Snake : Figure
     {
-
         Direction direction;
 
-        public Snake (Point tail, int lenght, Direction _direction)
+        public Snake( Point tail, int length, Direction _direction )
         {
-            pList = new List<Point>();
-
             this.direction = _direction;
-
-            for (int i = 0; i < lenght; i++)
+            pList = new List<Point>();
+            for ( int i = 0; i < length; i++ )
             {
-                Point p = new Point(tail);
-                p.Move(i, direction);
-
-                pList.Add(p);
+                Point p = new Point( tail );
+                p.Move( i, direction );
+                pList.Add( p );
             }
         }
 
         internal void Move()
         {
             Point tail = pList.First();
-            pList.Remove(tail);
+            pList.Remove( tail );
             Point head = GetNextPoint();
-            pList.Add(head);
+            pList.Add( head );
 
             tail.Clear();
             head.Draw();
@@ -40,33 +36,30 @@ namespace MySnake
         public Point GetNextPoint()
         {
             Point head = pList.Last();
-            Point nextPoint = new Point(head);
-            nextPoint.Move(1, direction);
+            Point nextPoint = new Point( head );
+            nextPoint.Move( 1, direction );
             return nextPoint;
         }
 
-        public void HandleKey (ConsoleKey key)
+        public void HandleKey(ConsoleKey key)
         {
-            if (key == ConsoleKey.LeftArrow)
+            if ( key == ConsoleKey.LeftArrow )
                 direction = Direction.LEFT;
-
-            else if (key == ConsoleKey.RightArrow)
+            else if ( key == ConsoleKey.RightArrow )
                 direction = Direction.RIGHT;
-
-            else if (key == ConsoleKey.UpArrow)
-                direction = Direction.UP;
-
-            else if (key == ConsoleKey.DownArrow)
+            else if ( key == ConsoleKey.DownArrow )
                 direction = Direction.DOWN;
+            else if ( key == ConsoleKey.UpArrow )
+                direction = Direction.UP;
         }
 
-        internal bool Eat (Point food)
+        internal bool Eat( Point food )
         {
             Point head = GetNextPoint();
-            if (head.IsHit(food))
+            if ( head.IsHit( food ) )
             {
                 food.sym = head.sym;
-                pList.Add(food);
+                pList.Add( food );
                 return true;
             }
             else
