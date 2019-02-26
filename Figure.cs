6433e6388a64d6cad144619effdf25425e68efc4
constant-i <constant-i@yandex.ru>
2019-02-26 21:51:57 +0300

    метод Draw теперь можно переопределять

--- Figure.cs
+++ Figure.cs
@@ -10,7 +10,7 @@ namespace MySnake
     {
         protected List<Point> pList;
 
-        public void Draw()
+        public virtual void Draw()
         {
             foreach (Point p in pList)
             {
