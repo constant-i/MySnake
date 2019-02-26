babc3f5f2662d9dc524de56ff8f83705e4f125e6
constant-i <constant-i@yandex.ru>
2019-02-26 22:00:53 +0300

    в теле main упражнения, показывающие суть полиморфизма

--- Program.cs
+++ Program.cs
@@ -23,7 +23,45 @@ namespace MySnake
             rightLine.Draw();
             leftLine.Draw();
 
-            Point p = new Point(4, 5, '*');
+
+            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            //Draw(v1);
+
+            Point p = new Point(4, 5, '#');
+
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            //Draw(fSnake);
+
+            Snake snake = (Snake) fSnake;
+            //Draw(snake);
+
+            
+            
+
+
+            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
+
+            List<Figure> figures = new List<Figure>();
+
+            figures.Add(snake);
+            figures.Add(v1);
+            figures.Add(h1);
+
+            foreach (/*var*/Figure f in figures)
+            {
+                f.Draw();
+            }
+
+            Thread.Sleep(200);
+            snake.Move();
+            Thread.Sleep(200);
+            snake.Move();
+            Thread.Sleep(200);
+            snake.Move();
+
+            Console.Read();
+
+            /*Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
@@ -52,7 +90,13 @@ namespace MySnake
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-            }
+            }*/
         } 
+
+
+        static void Draw (Figure figure)
+        {
+            figure.Draw();
+        }
     }
 }
