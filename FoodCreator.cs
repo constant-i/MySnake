51838e526ddaee8ccee7fb243a97d6a996f9203b
constant-i <constant-i@yandex.ru>
2019-02-26 20:26:06 +0300

    привел код в точное соответствие с кодом у преподавателя,  начиная от порядка операторов, написания имён переменных и пробелов в сторках

--- FoodCreator.cs
+++ FoodCreator.cs
@@ -8,25 +8,24 @@ namespace MySnake
 {
     class FoodCreator
     {
-        int mapWidth;
+        int mapWidht;
         int mapHeight;
         char sym;
 
-        Random random = new Random();
+        Random random = new Random( );
 
-        public FoodCreator (int mapWidth, int mapHeight, char sym)
+        public FoodCreator(int mapWidth, int mapHeight, char sym)
         {
-            this.mapWidth = mapWidth;
+            this.mapWidht = mapWidth;
             this.mapHeight = mapHeight;
             this.sym = sym;
         }
 
         public Point CreateFood()
         {
-            int x = random.Next(2, mapWidth - 2);
-            int y = random.Next(2, mapHeight - 2);
-            return new Point(x, y, this.sym);
+            int x = random.Next( 2, mapWidht - 2 );
+            int y = random.Next( 2, mapHeight - 2 );
+            return new Point( x, y, this.sym );
         }
-
     }
 }
