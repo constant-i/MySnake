7892c5d7802e38560afc8859ccbf045eca9d9ca5
constant-i <constant-i@yandex.ru>
2019-02-28 17:33:34 +0300

    в класс Змейка добавлен метод проверки ударения с хвостом (змейка укусила сама себя)

--- Snake.cs
+++ Snake.cs
@@ -41,6 +41,17 @@ namespace MySnake
             return nextPoint;
         }
 
+        internal bool IsHitTail()
+        {
+            Point head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
+
         public void HandleKey(ConsoleKey key)
         {
             if ( key == ConsoleKey.LeftArrow )
