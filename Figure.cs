21a02bda9b471bd047d1e0a5984ab05a9bd8bb38
constant-i <constant-i@yandex.ru>
2019-02-28 17:37:09 +0300

    добавленны два метода: - проверка, что данная фигура имеет ударилась с другой, переданной в аргументе фигурой (имеет общие точки) - проверка, что данная фигура ударилась с переданной в аргументе точкой

--- Figure.cs
+++ Figure.cs
@@ -17,5 +17,26 @@ namespace MySnake
                 p.Draw();
             }
         }
+
+        internal bool IsHit (Figure figure)
+        {
+            foreach (Point p in pList)
+            {
+                if (figure.IsHit(p))
+                    return true;
+            }
+            return false;
+        }
+
+        internal bool IsHit (Point point)
+        {
+            foreach  (Point p in pList)
+            {
+                if (p.IsHit(point))
+                    return true;
+            }
+            return false;
+        }
+
     }
 }
